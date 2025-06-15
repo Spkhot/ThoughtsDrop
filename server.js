8567877db3685adbdab3fba27792e379d22110ad
soham khot <sohamkhot75@gmail.com>
2025-06-15 15:03:31 +0530

    Update server

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+ 
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ Connected to MongoDB'))
+.catch((err) => console.error('❌ MongoDB error:', err));
 
 // Middleware
 app.use(cors());
