30b86c4fe7d77343fc30354ca3f9847f5ce35454
soham khot <sohamkhot75@gmail.com>
2025-06-15 15:05:54 +0530

    Update server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,9 +7,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
  
-require('dotenv').config();
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
